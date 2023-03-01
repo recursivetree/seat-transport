fc1e1be5658efdff9b547969290d832296dff099
recursive_tree <recursivetreemail@gmail.com>
2023-03-01 13:02:48 +0100

    add colors to reward/collateral

--- src/resources/views/costs.blade.php
+++ src/resources/views/costs.blade.php
@@ -12,12 +12,14 @@
             </h5>
             <div class="card-text my-3 mx-3">
                 <dl>
-                    <dt>Collateral</dt>
-                    <dd>{{ number($collateral,0) }} ISK</dd>
                     <dt>Volume</dt>
                     <dd>{{ number($volume) }} m<sup>3</sup></dd>
                     <dt>Reward</dt>
-                    <dd>{{ number($cost, 0) }} ISK</dd>
+                    <dd class="text-success">{{ number($cost, 0) }} ISK</dd>
+                    <dt>Collateral</dt>
+                    <dd class="text-danger">{{ number($collateral,0) }} ISK</dd>
+                    <dt>Destination</dt>
+                    <dd>{{ $route->destination_location()->name }}</dd>
                     <dt>Info</dt>
                     <dd>{{ $route->info_text ?? "Your administrator can put a text about how to create the contract here." }}</dd>
                 </dl>
